d673cec17ee3337dc9ed60c3a3e6f0a1eaaa2c95
Francis Nicholas Ramos <pfrancisnicholasramos@gmail.com>
2025-03-19 14:39:04 +0800

    Tweak content positioning

--- my-app/app/components/LandingContent.tsx
+++ my-app/app/components/LandingContent.tsx
@@ -1,26 +1,45 @@
 import React from "react";
+import Image from "next/image";
 import { FaArrowRight } from "react-icons/fa";
-import Button from "./Button";
+import Button from "./Button"; // custom reusable button
+import Puppy from '@/assets/images/puppy.png';
 
 const LandingContent = () => {
   return (
     <div>
-      <div>
-        <h2 className="text-[28px]">LOREM IPSUM DOLORES</h2>
+      <div className="flex flex-col mx-[125px] py-[100px] gap-[100px]">
+        <div className="max-w-[700px] mb-[-80]">
+          <h2 className="text-[28px]">LOREM IPSUM DOLORES</h2>
 
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco Lorem ipsum dolor sit
-          amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-          exercitation ullamco
-        </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+            minim veniam, quis nostrud exercitation ullamco Lorem ipsum dolor sit
+            amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco
+          </p> 
+          
+          <br/>
+          
+          <Button variant="primary" className="rounded-[75px] py-[10px] px-[25px] gap-[20px]">
+            PawPal Program <FaArrowRight />
+          </Button>
+        </div>
 
-        <Button variant="primary"> 
-          PawPal Program <FaArrowRight />
-        </Button>
-        
+        <div className="flex items-center max-w-[700px]">
+          <Image src={Puppy} width={700} height={400} alt="puppy" className="object-cover"/>
+
+          <div>
+            <h1 className="text-[#AFAFAF] pb-2">UT ENIM AD MINIM VENIAM</h1>
+            <h2 className="text-[#344B50] font-medium pb-2">Lorem Ipsum Dolor Sit Amet</h2> 
+            <p className="text-[#9D9595] pb-2">Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud</p>
+  
+            <Button variant='primary' className=' rounded-[25px] py-[5px] px-[10px]'>
+              FOSTER ME <FaArrowRight />
+            </Button>
+          </div>
+        </div>
       </div>
     </div>
   );
