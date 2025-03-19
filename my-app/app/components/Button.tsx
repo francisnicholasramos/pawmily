91a92b1995f4d327e49a954d5bb80cc271a39b18
Francis Nicholas Ramos <pfrancisnicholasramos@gmail.com>
2025-03-19 14:34:05 +0800

    Fix custom styles for reusable button

--- my-app/app/components/Button.tsx
+++ my-app/app/components/Button.tsx
@@ -1,22 +1,22 @@
 import React from "react";
-import "../../assets/styles/button.scss";
 
 interface ButtonProps {
   variant?: 'primary';
+  className?: string;
   children: React.ReactNode;
 }
 
-const Button = ({ variant = 'primary', children }: ButtonProps) => {
+const Button = ({ variant = 'primary', children, className }: ButtonProps) => {
   const variants = {
-    primary: "flex items-center space-x-[5px] bg-[#334a4f] text-[#F9DAC6FA] rounded-[75px] px-[20px] py-[20px]",
+    primary: "flex items-center bg-[#344B50] font-normal hover:bg-[#47656c] text-[#F9DAC6FA]  cursor-pointer shadow-[0px_4px_3px_rgba(0,0,0,0.25)]",
   };
 
   return (
-    <>
-      <button className={`${variants[variant]}`}>
+    <div className="button-container">
+      <button className={`${variants[variant]} ${className}`}>
         {children}
       </button>
-    </>
+    </div>
   );
 };
 
