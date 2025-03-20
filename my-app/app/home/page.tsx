3a1636dce309567f0acb9da3aec6eb9ac7faff8c
Francis Nicholas Ramos <pfrancisnicholasramos@gmail.com>
2025-03-20 16:26:56 +0800

    Add component

--- my-app/app/home/page.tsx
+++ my-app/app/home/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import DirectoryLinks from '../components/DirectoryLinks'
-import LandingContent from '../components/LandingContent'
+import LandingContent from '../components/LandingPage/LandingContent'
 
 const page = () => {
   return (
