aed704c940fd61360bb95270db365b3a76717183
Francis Nicholas Ramos <pfrancisnicholasramos@gmail.com>
2025-03-23 22:05:56 +0800

    Simplified and shorten card data properties

--- my-app/app/paw-match/cardData.tsx
+++ my-app/app/paw-match/cardData.tsx
@@ -1,77 +1,28 @@
-import Blue1 from "@/assets/images/blue1.jpg";
-import Blue2 from "@/assets/images/blue2.jpg";
-import Blue3 from "@/assets/images/blue3.jpg";
-import Blue4 from "@/assets/images/blue4.jpg";
-import Blue5 from "@/assets/images/blue5.jpg";
-import Visi from "@/assets/images/visi.jpg";
-import Winter from "@/assets/images/winter.jpg";
-import Shadow from "@/assets/images/shadow.jpg"
+type Card = {
+  id: number;
+  image: string;
+}
 
-const card = [
+const card: Card[] = [
   {
-    image: Blue4.src,
-    name: "BlueDog",
-    breed: "Siklistang Abnoy",
-    width: 400,
-    height: 550,
-    alt: "pets",
+    id: 1,
+    image:
+      "https://images.unsplash.com/photo-1679363336771-ac19093981ee?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
-    image: Visi.src,
-    name: "Visi(tor)",
-    breed: "Calico",
-    width: 400,
-    height: 550,
-    alt: "pets",
+    id: 2,
+    image:
+      "https://images.unsplash.com/photo-1608144252993-f33f23e9df9a?q=80&w=2102&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
-    image: Blue1.src,
-    name: "Blue",
-    breed: "Siberian Monkey",
-    width: 400,
-    height: 550,
-    alt: "pets",
+    id: 3,
+    image:
+      "https://images.unsplash.com/photo-1496293455970-f8581aae0e3b?q=80&w=2013&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
-    image: Winter.src,
-    name: "Winter",
-    breed: "White Summer",
-    width: 400,
-    height: 550,
-    alt: "pets",
-  },
-  {
-    image: Blue3.src,
-    name: "Porman",
-    breed: "Aspin",
-    width: 400,
-    height: 550,
-    alt: "pets",
-  },
-  {
-    image: Shadow.src,
-    name: "Shadow",
-    breed: "Aspin",
-    width: 400,
-    height: 550,
-    alt: "pets",
-  },
-
-  {
-    image: Blue5.src,
-    name: "Cuteness Overload",
-    breed: "Samoyed",
-    width: 400,
-    height: 550,
-    alt: "pets",
-  },
-  {
-    image: Blue2.src,
-    name: "Blue",
-    breed: "Siberian Husky",
-    width: 400,
-    height: 550,
-    alt: "pets",
+    id: 4,
+    image:
+      "https://images.unsplash.com/photo-1572809596155-39da9e985f22?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 ];
 
