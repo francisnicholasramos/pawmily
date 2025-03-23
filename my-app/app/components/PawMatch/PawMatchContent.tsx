b8a4dfcd9fccaabc3871f28454573fe4a6ac7350
Francis Nicholas Ramos <pfrancisnicholasramos@gmail.com>
2025-03-23 22:01:01 +0800

    Refactored: From swiper to framer motion

--- my-app/app/components/PawMatch/PawMatchContent.tsx
+++ my-app/app/components/PawMatch/PawMatchContent.tsx
@@ -1,58 +1,83 @@
 "use client";
-import React from "react";
-import Image from "next/image";
+import React, { useState } from "react";
 import { FaHeart } from "react-icons/fa";
 import { MdClose } from "react-icons/md";
-
 import cardData from "@/app/paw-match/cardData";
 import Button from "../Button";
-
-import SlapContainer from "@/assets/images/slap-container.png";
-import PassContainer from "@/assets/images/pass-container.png";
-import Slap from "@/assets/images/pawHeart.png";
-import Pass from "@/assets/images/pass.png";
-
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/effect-cards"; // Ensure correct styles are imported
-import { EffectCards } from "swiper/modules";
+import { motion, useMotionValue, useTransform } from "framer-motion";
 
 const PawMatchContent = () => {
+  const [cards, setCards] = useState<Card[]>(cardData);
+
   return (
-    <div className="flex justify-center mx-[125px] py-[125]">
-      <div>
-        <Swiper
-          effect={"cards"}
-          grabCursor={true}
-          modules={[EffectCards]}
-          className="w-[400px] h-[600px] max-w-full"
-        >
-          {cardData.map((card, index) => (
-            <SwiperSlide>
-              <Image
-                key={index}
-                src={card.image}
-                alt={card.alt}
-                width={card.width}
-                height={card.height}
-                className="rounded-lg"
-              />
-            </SwiperSlide>
-          ))}
-        </Swiper>
+    <div className="grid place-items-center mx-[125px] py-[125]">
+      {cards.map((card) => {
+        return (
+          <Card key={card.id} cards={cards} setCards={setCards} {...card} />
+        );
+      })}
 
-        <div className="flex justify-center space-x-10 mt-4">
-          <Button className="rounded-full p-[20]">
-            <MdClose className="text-2xl"/>
-          </Button>
+      <div className="flex justify-center space-x-10 mt-10">
+        <Button className="rounded-full p-[20]">
+          <MdClose className="text-2xl" />
+        </Button>
 
-          <Button className="rounded-full p-[20]">
-            <FaHeart className="text-2xl"/>
-          </Button>
-        </div>
+        <Button className="rounded-full p-[20]">
+          <FaHeart className="text-2xl" />
+        </Button>
       </div>
-
     </div>
+  );
+};
+
+const Card = ({
+  id,
+  image,
+  cards,
+  setCards,
+}: {
+  id: number;
+  image: string;
+
+  setCards: Dispatch<SetStateAction<Card[]>>;
+  cards: Card[];
+}) => {
+  const x = useMotionValue(0);
+  const opacity = useTransform(x, [-150, 0, 150], [0, 1, 0]);
+  const rotateRaw = useTransform(x, [-150, 150], [-18, 18]);
+
+  const isFront = id === cards[cards.length - 1].id;
+
+  const rotate = useTransform(() => {
+    const offset = isFront ? -1 : id % 2 ? 6 : -6;
+    return `${rotateRaw.get() + offset}deg`;
+  });
+
+  const handleDragEnd = () => {
+    if (Math.abs(x.get()) > 50) {
+      setCards((pv) => pv.filter((v) => v.id !== id));
+    }
+  };
+
+  return (
+    <motion.img
+      src={image}
+      className="h-96 w-110 object-cover rounded-lg hover:cursor-grab active:cursor-grabbing"
+      style={{
+        gridRow: 1,
+        gridColumn: 1,
+        x,
+        opacity,
+        rotate,
+        transition: "0.125s transform",
+      }}
+      drag="x"
+      dragConstraints={{
+        left: 0,
+        right: 0,
+      }}
+      onDragEnd={handleDragEnd}
+    />
   );
 };
 
