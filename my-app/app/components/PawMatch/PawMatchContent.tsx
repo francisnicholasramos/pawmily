36ae54ab168daef1e4047b4388d760b52340e98a
Francis Nicholas Ramos <pfrancisnicholasramos@gmail.com>
2025-03-25 15:29:11 +0800

    Removed inline component

--- my-app/app/components/PawMatch/PawMatchContent.tsx
+++ my-app/app/components/PawMatch/PawMatchContent.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import { FaHeart } from "react-icons/fa";
 import { MdClose } from "react-icons/md";
+import Card from "./Card";
 import cardData from "@/app/paw-match/cardData";
 import Button from "../Button";
 import { motion, useMotionValue, useTransform } from "framer-motion";
@@ -27,57 +28,6 @@ const PawMatchContent = () => {
         </Button>
       </div>
     </div>
-  );
-};
-
-const Card = ({
-  id,
-  image,
-  cards,
-  setCards,
-}: {
-  id: number;
-  image: string;
-
-  setCards: Dispatch<SetStateAction<Card[]>>;
-  cards: Card[];
-}) => {
-  const x = useMotionValue(0);
-  const opacity = useTransform(x, [-150, 0, 150], [0, 1, 0]);
-  const rotateRaw = useTransform(x, [-150, 150], [-18, 18]);
-
-  const isFront = id === cards[cards.length - 1].id;
-
-  const rotate = useTransform(() => {
-    const offset = isFront ? -1 : id % 2 ? 6 : -6;
-    return `${rotateRaw.get() + offset}deg`;
-  });
-
-  const handleDragEnd = () => {
-    if (Math.abs(x.get()) > 50) {
-      setCards((pv) => pv.filter((v) => v.id !== id));
-    }
-  };
-
-  return (
-    <motion.img
-      src={image}
-      className="h-96 w-110 object-cover rounded-lg hover:cursor-grab active:cursor-grabbing"
-      style={{
-        gridRow: 1,
-        gridColumn: 1,
-        x,
-        opacity,
-        rotate,
-        transition: "0.125s transform",
-      }}
-      drag="x"
-      dragConstraints={{
-        left: 0,
-        right: 0,
-      }}
-      onDragEnd={handleDragEnd}
-    />
   );
 };
 
